1d5ce9e52f58bceb8b578d70d41cab5cc93b1a71
EpicVon2468 <thomaschad0909@gmail.com>
2026-05-02 13:35:10 +1000

    Minor breaking changes

--- src/bin/jvm.rs
+++
@@ -1,9 +0,0 @@
-//! The `fuji-jvm` binary.  Equivalent to running `fuji manage jvm`.
-use anyhow::Result;
-
-use fuji::alias_entrypoint;
-
-/// `fuji-jvm`
-pub fn main() -> Result<()> {
-	alias_entrypoint(&["manage".into(), "jvm".into()])
-}
--- src/bin/kt.rs
+++
@@ -1,9 +0,0 @@
-//! The `fuji-kt` binary.  Equivalent to running `fuji manage kotlin`.
-use anyhow::Result;
-
-use fuji::alias_entrypoint;
-
-/// `fuji-kt`
-pub fn main() -> Result<()> {
-	alias_entrypoint(&["manage".into(), "kotlin".into()])
-}
